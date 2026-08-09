c7110c231a6f6308bb9cf54986861fc533b1140d
naoto3815 <naoto3815@gmail.com>
2026-08-09 16:57:27 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "ironlog-v3";
+const CACHE = "ironlog-v4";
 const ASSETS = ["./", "./index.html", "./app.js", "./manifest.json", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
